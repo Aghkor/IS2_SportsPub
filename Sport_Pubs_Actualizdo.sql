3413e0b612349d22177ddce82c84f4824a9ef31a
Aghkor <45183494+Aghkor@users.noreply.github.com>
2019-03-10 18:16:11 -0500

    Update Sport_Pubs_Actualizdo.sql

--- Sport_Pubs_Actualizdo.sql
+++ Sport_Pubs_Actualizdo.sql
@@ -1,7 +1,7 @@
 create database Sport_Pub;
 use Sport_Pub;
 create table Usuario(
-	IDU INTEGER primary key,
+	IDU SERIAL primary key,
 	Nombre VARCHAR(30) NOT NULL,
 	Apellido VARCHAR(30) NOT NULL,
 	Edad INTEGER NOT NULL,
@@ -9,22 +9,11 @@ create table Usuario(
 	Email VARCHAR(60) NOT NULL,
 	Celular INTEGER NOT NULL,
 	Usuario VARCHAR(60) NOT NULL,
-	Contraseña VARBINARY(60) NOT NULL
+	Contrasena VARBINARY(60) NOT NULL
 );
-create table Usuario(
-	IDU INTEGER primary key,
-	Nombre VARCHAR(30) NOT NULL,
-	Apellido VARCHAR(30) NOT NULL,
-	Edad INTEGER NOT NULL,
-	genero VARCHAR(40) NOT NULL,
-	Fecha DATE,
-	Email VARCHAR(60) NOT NULL,
-	Celular INTEGER NOT NULL,
-	Usuario VARCHAR(60) NOT NULL,
-	Contraseña VARCHAR(60) NOT NULL
-);
+
 create table Equipo(
-	IDE INTEGER primary key NOT NULL,
+	IDE SERIAL primary key NOT NULL,
 	Nombre VARCHAR(100)  NOT NULL,
 	Fecha_Creacion INTEGER NOT NULL,
 	Lider_Equipo VARCHAR(69) NOT NULL,
@@ -32,13 +21,13 @@ create table Equipo(
 	Cantidad_de_jugador INTEGER NOT NULL
 );
 create table Usuario_Equipo(
-	IDUE INTEGER PRIMARY KEY,
+	IDUE SERIAL PRIMARY KEY,
 	IDU INTEGER  REFERENCES Usuario(IDU),
 	IDE INTEGER REFERENCES Equipo(IDE)
 );
 
 create table Encuentro(
-	IDP int primary key ,
+	IDP SERIAL primary key ,
 	Equipo1 VARCHAR(80) NOT NULL,
 	Equipo2 VARCHAR(80) NOT NULL,
 	IDE1 INTEGER refereNces Equipo(IDE) NOT NULL ,
@@ -48,49 +37,49 @@ create table Encuentro(
 	TIEMPO_ESPERA NOT NULL	
 );
 create table Encuentro_Usuario(
-	 IDEU INTEGER NOT NULL,
+	 IDEU SERIAL NOT NULL,
 	 IDU INTEGER  REFERENCES Usuario(IDU),
          IDP INTEGER REFERENCES Comentario(IDP)
 );
 create table Encuentro_Equipo(
-	 IDEU INTEGER NOT NULL,
+	 IDEU SERIAL NOT NULL,
 	 IDE INTEGER  REFERENCES Usuario(IDE),
          IDP INTEGER REFERENCES Comentario(IDP)
 );
 create table Comentario(
-	IDC INTEGER primary key NOT NULL,
+	IDC SERIAL primary key NOT NULL,
 	Comentarios VARCHAR(60) NOT NULL
 );
 
 create table ValorizacionJugador(
-	IDVJ INTEGER primary key,
+	IDVJ SERIAL primary key,
 	Valorizacion VARCHAR(5) NOT NULL,
-	IDU INTEGER REFERENCES Usuario (IDU)
+	IDU SERIAL REFERENCES Usuario (IDU)
 );
 
 create table valorizacionEquipo(
-	IDVE INTEGER primary key NOT NULL,
+	IDVE SERIAL primary key NOT NULL,
 	Valorizacion VARCHAR(5) NOT NULL,
-	IDE INTEGER REFERENCES Equipo(IDE)
+	IDE SERIAL REFERENCES Equipo(IDE)
 );
 create table valorizacionPartido(
-	IDVP INTEGER primary key NOT NULL,
+	IDVP SERIAL primary key NOT NULL,
 	Valorizacion VARCHAR(5) NOT NULL,
-	IDP INTEGER REFERENCES Encuentro(IDP)
+	IDP SERIAL REFERENCES Encuentro(IDP)
 );
 create table Comentario_Usuario(
-	IDCU INTEGER PRIMARY KEY NOT NULL,
-	IDU INTEGER  REFERENCES Usuario(IDU),
-        IDC INTEGER REFERENCES Comentario(IDC)
+	IDCU SERIAL PRIMARY KEY NOT NULL,
+	IDU SERIAL  REFERENCES Usuario(IDU),
+        IDC SERIAL REFERENCES Comentario(IDC)
 );
 create table Comentario_equipo(
-        IDCE INTEGER PRIMARY KEY NOT NULL,
-	IDE INTEGER  REFERENCES Equipo(IDE),
-        IDC INTEGER REFERENCES Comentario(IDC)
+        IDCE SERIAL PRIMARY KEY NOT NULL,
+	IDE SERIAL  REFERENCES Equipo(IDE),
+        IDC SERIAL REFERENCES Comentario(IDC)
 );
 
 create table Comentario_Encuentro(
-	IDCU INTEGER PRIMARY KEY NOT NULL,
-	IDP INTEGER  REFERENCES Encuentro(IDP),
-        IDC INTEGER REFERENCES Comentario(IDC)
+	IDCU SERIAL PRIMARY KEY NOT NULL,
+	IDP SERIAL  REFERENCES Encuentro(IDP),
+        IDC SERIAL REFERENCES Comentario(IDC)
 );
