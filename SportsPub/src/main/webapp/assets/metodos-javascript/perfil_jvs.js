7570e5fe12bb99679a305fe779e2d2bd4fc5eadb
PERSONAL <PERSONAL@Lenovo-PC>
2019-03-13 17:25:16 -0500

    JSON

--- SportsPub/src/main/webapp/assets/metodos-javascript/perfil_jvs.js
+++ SportsPub/src/main/webapp/assets/metodos-javascript/perfil_jvs.js
@@ -1,55 +1,44 @@
 console.log('correcto');
 
-
-document.querySelector('#contenedor').addEventListener('click',datosusuario);
-
-var json1= 'datoprueba.json';
-//var conversionjs= JSON.parse(json1);
-console.log(json1);
-var div1 = document.getElementById('usuario').value;
-var div2 = document.getElementById('nombre').value;
-var div3 = document.getElementById('apellido').value;
-var div4 = document.getElementById('email').value;
-var div5 = document.getElementById('celular').value;
-var div6 = document.getElementById('edad').value;
-var div7 = document.getElementById('genero').value;
-var div8 = document.getElementById('futbol5').value;
-var div9 = document.getElementById('futbol8').value;
+document.querySelector('#boton1').addEventListener('click',datosusuario);
 
 
-
-function datosusuario () {
-
-  const xhttp= new  XMLHttpRequest();
-  xhttp.open('GET',json1, true);
-  xhttp.send();
-  console.log('correcto 2');
-
-  xhttp.onreadystatechange = function () {
-
-    if(this.readyState===4 && this.status===200){
+function datosusuario(){
+const xhttp = new XMLHttpRequest();
+ xhttp.open("GET","datosprueba.json", true);
+ xhttp.send(); 
+ console.log('correcto2');
+ 
+ xhttp.onreadystatechange = function(){    
      
-      div1.innerHTML=conversionjs.persona.usuario.toString();
-      div2.innerHTML=conversionjs.persona.nombre.toString();
-      div3.innerHTML=conversionjs.persona.apellido.toString();
-      div4.innerHTML=conversionjs.persona.email.toString();
-      div5.innerHTML=conversionjs.persona.celular.toString();    
-      div8.innerHTML=conversionjs.equipofutbol5.nombrequipo.tostring();
-      div9.innerHTML=conversionjs.equipofutbol8.nombrequipo.tostring();
+    if(xhttp.status===200 && xhttp.readyState=== 4){
       
-      
-      console.log('correcto3');
-      
-      function cambiar(){        
-          div6.innerHTML=conversionjs.persona.edad.valueOf().toString();
-          div7.innerHTML=conversionjs.persona.genero.valueOf().toString();            
-          console.log('correcto4');
-          
-      }
-      
-    }  
+        
+        var jsonResponse = xhttp.responseText;
+        var objeto_json= JSON.parse(jsonResponse);
+        datousuario= objeto_json.datos.usuario.user;       
+       
+       for(var i=0; i<datosusuario.length;i++){
+           var nombreusuario = objeto_json.datos.usuario[i].nombre;
+           
+           if(nombreusuario !== null){
+               document.getElementById("usuario").innerHTML = nombreusuario;
+               
+               
+           }
+           
+       }
+        
+       
+        
+        
+    }
+        
+        
+    }
+   
     
-  };
-  
-  
-}
+};
+
+
+
