64016f98647e0934b9a2c48fcdc2089f10f7c06f
PERSONAL <PERSONAL@Lenovo-PC>
2019-05-29 13:36:26 -0500

    metodo js

--- SportsPub/src/main/webapp/assets/metodos-javascript/crearEq8.js
+++ SportsPub/src/main/webapp/assets/metodos-javascript/crearEq8.js
@@ -1,13 +1,9 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 document.addEventListener("DOMContentLoaded", init, true);
 
 function init(){
     document.querySelector("#cEquip8").addEventListener("click", handlePevent, true);
+    document.querySelector("#cEquip5").addEventListener("click", handlePevent, true);
     
     function handlePevent(e){
        e.preventDefault();
@@ -25,26 +21,32 @@ function init(){
        var txtjug6= document.getElementById("int_6");
        var txtjug7 = document.getElementById("int_7");
        var txtjug8=document.getElementById("int_8");
-       var userdata= '{"nombreequipo":'+txtnomeq.value + ',"lider_eq":' + txtlidereq.value + ',"tipo_futbol":' + txtTipoF.value +
-                       ',"genero":' + txtGen.txt + ',"num_jugadores":'+ txtNjug.value + ',"jug_1":' +txtjug1.value + 
-                       ',"jug_2":'+ txtjug2.value + ',"jug_3":'+ txtjug3.value + ',"jug_4":'+ txtjug4.value + ',"jug_5":' + txtjug5.value + 
-                       ',"jug_6":' + txtjug6.value + ',"jug_7":' + txtjug7.value + ',"jug_8":'+ txtjug8.value + '}';
+       var regequipo= '{"nombreequipo":'+txtnomeq.value + ',"lider_eq":' + txtlidereq.value + ',"tipo_futbol":' + txtTipoF.value +
+                       ',"genero":' + txtGen.txt + ',"num_jugadores":'+ txtNjug.value + '}';
                
-        Cequipo8ByAjax(userdata);
+       var equipojug= '{"jug1":' +txtjug1.value + ',"jug2":'+ txtjug2.value + ',"jug3":'+ txtjug3.value + ',"jug4":'+ txtjug4.value + ',"jug5":' + txtjug5.value + 
+                        ',"jug6":' + txtjug6.value + ',"jug7":' + txtjug7.value + ',"jug8":'+ txtjug8.value + '}';
+               
+       
+               
+        CequipoByAjax(regequipo);
+        CequipoByAjax(equipojug);
        
     }
     
-    function Cequipo8ByAjax(data){
+    function CequipoByAjax(data1,data2){
         var request = new XMLHttpRequest();
-        request.open("post",Servletcrear_equipo8, true);
+        request.open("post","Servletequipo", true);
         request.setRequestHeader("Content-type","application/x-www-form-urlencoded");
         request.onreadystatechange = function (){
             //si la respuesta fue exitosa
             if(request.readyState === 4 && request.status === 200){
                 var message = request.responseText;
-                if(message ===   "Partido correctamente registrado "){
+                if(message ===   "Equipo  correctamente registrado "){
                     alert(message);
                     document.location.href = 'perfilequipo8.jsp';
+                    
+                    
                 }else{
                     
                 }alert(message+" intente nuevamente");
@@ -55,7 +57,8 @@ function init(){
 				alert(request.statusText);
             }
         };
-        request.send("encuentrodata=" + data);
+        request.send("equipodata=" + data1);
+        request.send("jData =" + data2);
     }
     
 }
